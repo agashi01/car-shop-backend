11d47af2bb5737f38aa7c7db7900715af453aa8d
Andi Gashi <andigashi2005@gmail.com>
2026-04-04 21:14:49 +0200

    supabase url

--- src/db.js
+++ src/db.js
@@ -8,7 +8,7 @@ const db = knex({
     user: process.env.PG_USERNAME,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
-    ssl: { rejectUnauthorized: true }
+    ssl: { rejectUnauthorized: false }
   },
 });
 
