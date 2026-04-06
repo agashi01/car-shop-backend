44d3eeeacf38b2b9afa7ee1eb3997572125745eb
Andi Gashi <andigashi2005@gmail.com>
2026-04-06 23:02:33 +0200

    test logs

--- src/cars.js
+++ src/cars.js
@@ -516,7 +516,17 @@ const readAll = (db) => async (req, res) => {
       id,
       dealer
     );
-    return res.status(200).json(cars);
+
+    const shuffle = (array) => {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    }
+
+
+    return res.status(200).json(shuffle(cars));
   } catch (err) {
     console.log(err);
     res.status(400).json("something went wrong");
